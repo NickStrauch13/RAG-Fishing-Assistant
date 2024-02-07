2441c95e28faf0d2d7de29e720033886b54dd781
Nick Strauch <njs35@duke.edu>
2024-02-07 12:51:41 -0500

    updated tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,20 @@
-from src.main import func
+from src.aws_utils import connect_to_database, query_db
 
 
-def test_func():
-    assert func(3) == 4
-    assert func(4) == 5
-    assert func(-1) == 0
+def test_connect_to_database():
+    conn, cursor = connect_to_database()
+    assert conn is not None
+    assert cursor is not None
+    cursor.close()
+    conn.close()
+
+
+def test_query_db():
+    conn, cursor = connect_to_database()
+    query = "SELECT * FROM raw_paragraphs LIMIT 10"
+    records = query_db(cursor, query)
+    assert records is not None
+    assert type(records) == tuple
+    assert len(records) == 10
+    cursor.close()
+    conn.close()
