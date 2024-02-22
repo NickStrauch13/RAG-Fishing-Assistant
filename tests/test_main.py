7e9901c9ca569773ce9fca7fb9a4d04b25374382
Nick Strauch <njs35@duke.edu>
2024-02-21 19:48:47 -0500

    Configured client in test

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,14 +1,18 @@
 from src.embeddings_utils import embed_text_openai 
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
 def test_text_embedding():
     """
     Test the text embedding function.
     """
-    # Define the input text
+    # Create client and test string
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
     text = "This is a test."
-    # Embed the text
-    embedding = embed_text_openai(text)
+    embedding = embed_text_openai(client, text)
     # Check the type of the output
     assert type(embedding) == list
     # Check the length of the output
